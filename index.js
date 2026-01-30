192775bada8aa695d7df4a46d3efc2ee30de452d
Usef <yosefarafa198@gmail.com>
2026-01-30 15:56:55 +0300

    feat: init server

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+const app = express();
+app.get("/", async (req, res, next) => {
+  const query = req.query.username;
+  const data = await fetch(`https://users.roblox.com/v1/usernames/users`, {
+    method: "POST",
+    body: JSON.stringify({
+      usernames: [query],
+      execludeBannedUsers: false,
+    }),
+  }).then((res) => res.json());
+  const avatarImg = await fetch(
+    `https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=${data?.data?.[0]?.id}&format=Png&size=150x150`
+  ).then((res) => res.json());
+  try {
+    res.status(200).json({ data: data?.data, avatarImg });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.message, err.stack);
+});
+
+app.listen(3000, () => console.log("server runinng"));
